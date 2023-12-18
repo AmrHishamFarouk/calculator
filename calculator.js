7a084b8fafb2795155aba296b05dc6241056db4f
AmrHishamFarouk <hishamamr121@gmail.com>
2023-12-18 23:23:48 +0200

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,1 +1,36 @@
-console.log("try");
+let no1 = 0.0,
+    no2 = 0.0,
+    op = '+';
+
+function operate(no1,no2,op){
+    switch(op){
+        case '+':
+            add(no1,no2);
+            break;
+        case '-':
+            subtract(no1,no2);
+            break;
+        case 'x':
+            multiply(no1,no2);
+            break;
+        case '/':
+            divide(no1,no2);
+            break;
+    }
+}
+
+
+
+
+function add(x,y){
+    return x+y;
+}
+function subtract(x,y){
+    return x-y;
+}
+function multiply(x,y){
+    return x*y;
+}
+function divide(x,y){
+    return x/y;
+}
