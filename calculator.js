367997cd93485e2b845e4636df983d1fac7facd4
AmrHishamFarouk <hishamamr121@gmail.com>
2024-01-02 23:06:42 +0200

    Add files via upload

--- calculator.js
+++ calculator.js
@@ -1,47 +1,57 @@
 let no1 = 0.0,
     no2 = 0.0,
-    op = '+'
-    onscreen = '';
+    op = '+',
+    onscreen = '',
+    result = 0;
 
 
 
 let screen = document.querySelector('.screen');
 
-let buttons = document.querySelectorAll('button');
+let buttons = document.querySelectorAll('.number');
 
 buttons.forEach(element => {
     element.addEventListener('click',() =>{
         fillscreen(element.textContent);
     });
-
 });
+
+let equal = document.querySelector('.equal');
+equal.addEventListener('click',() =>{
+    operate(no1,no2,op);
+    fillscreen(result);
+})
+
+
+
 
 function fillscreen(no){
     onscreen = onscreen + no;
-    
+    if(screen.firstChild){
+        const span = screen.firstChild;
+        screen.removeChild(span);
+    }
+
     const span = document.createElement('span');
     span.textContent = onscreen;
+    // span.style.cssText = 'color: blue; background: white;';     
     screen.appendChild(span);
-    // screen.removeChild(span);
 
 }
-
-
-
 
 function operate(no1,no2,op){
     switch(op){
         case '+':
-            add(no1,no2);
+            result = add(no1,no2);
             break;
         case '-':
-            subtract(no1,no2);
+            result = subtract(no1,no2);
             break;
         case 'x':
-            multiply(no1,no2);
+            result = multiply(no1,no2);
             break;
         case '/':
-            divide(no1,no2);
+            result = divide(no1,no2);
             break;
     }
 }
@@ -58,3 +68,16 @@ function multiply(x,y){
 function divide(x,y){
     return x/y;
 }
+
+
+
+
+Number("10");          // returns 10
+Number(" 10  ");       // returns 10
+Number("10.33");       // returns 10.33
+
+parseFloat("10");        // returns 10
+parseFloat("10.33");     // returns 10.33
+parseFloat("10 20 30");  // returns 10
+parseFloat("10 years");  // returns 10
+parseFloat("years 10");  // returns NaN
